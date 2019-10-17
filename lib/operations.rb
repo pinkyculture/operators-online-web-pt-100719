e077adf87f7cca9100e2c21b25ebd28f645588a1
daniel arzayus <randomblue1@yahoo.com>
2019-10-17 00:39:17 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+if speed > 60  
+  return unsafe?
+  else speed < 40
+    return unsafe?
+  
 end
-
+end 
 
 
 def not_safe?(speed)
-	
+	#conditional ? action_if_true : action_if_false
 end
 	
 
